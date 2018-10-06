2fc5cf00b7adc408810246c654d4280867cb4a22
sunfuhao <sfhgr25852@163.com>
2018-10-06 19:25:57 +0800

    new

--- nodejs/server/router.js
+++ nodejs/server/router.js
@@ -3,6 +3,8 @@ var pool = require("./sql")
 var url = require("url")
 var name_biao = "userinfo";
 var request = require("request")
+var search_json = require("./search_json")
+console.log(search_json)
 var wxConfig = {
 	AppID:"",
 	Secret:""
@@ -116,14 +118,17 @@ router.get('/cha', function (req, res) {
 	var param = req.query;
 	keys = ''
 	allkey = Object.keys(param)
-	allkey.map(function(x,i,me){
-		if(i<me.length-1){
-			keys += x + "=" + param[x] +" and "
-		}else{
-			keys += x + "=" + param[x]
-		}
-	})
-	var  sql_cha = "select * from " + name_biao + " WHERE "  +keys 
+
+	console.log(param)
+	let _id = "id="+param['id']
+	let _sex = "sex="+param['sex']
+	let _age = " age >= " + search_json['search_age'][param['age']][0] + " and  age <= " +  search_json['search_age'][param['age']][1]
+	let _height = " height >= " + search_json['search_height'][param['height']][0] + " and  height <= " +  search_json['search_height'][param['height']][1]
+	let housearea = " housearea  like'%" + param['old_area'] +"%'"
+	let currentarea = " currentarea  like '%" + param['current_area'] +"%'"
+	var  sql_cha = "select * from " + name_biao + " WHERE "  + _sex + ' and ' + _age + ' and ' + _height + ' and ' + housearea + ' and ' + currentarea
+	//var  sql_cha = "select * from " + name_biao + " WHERE "  + _id +' and ' + _sex + ' and ' + _age + ' and ' + _height
+	console.log(sql_cha)
 	pool(sql_cha, [1], function(err,results,fields){  
 		if(err){
 	    	console.log(err)
@@ -132,11 +137,14 @@ router.get('/cha', function (req, res) {
 	    res.send({data:results,msg:'cha',code:0})
 	});
 });
-router.get('/cha_ziji', function (req, res) {
-	pool("select * from userinfo ", [1], function(err,results,fields){  
+router.get('/cha_by_id', function (req, res) {
+	var param = req.query;
+	let _id = "id="+param['id']
+	var  sql_cha = "select * from " + name_biao + " WHERE " + _id
+	pool(sql_cha, [1], function(err,results,fields){  
 	    //do something  
 	    console.log(results)
-	      res.send({data:results,msg:'ok'})
+	    res.send({data:results,msg:'cha_by_id',code:0})
 
 	});
  userdetail = {id:'1',nickname:'孙好',realname:'',sex:1,housearea:'河北',currentarea:'beijing',age:24,height:174,study:'本科',school:'>河北建工',iphone:'13124793355',wechat:'13124793355',isay:' 我是一个人真的是',icon:'http://t11.baidu.com/it/u=1934927245,1175704773&fm=173&s=BB9445805E1876C670815DC1030050B8&w=640&h=446&img.JPEG',imglist:['http://t10.baidu.com/it/u=3994952046,3665680125&fm=173&s=51127591107D5B8E227865F30300D0B3&w=640&h=396&img.JPEG','http://t10.baidu.com/it/u=3231088869,2392068320&fm=173&s=A51216D18D034A4F5CBAEFD9030080BF&w=640&h=446&img.JPEG'],power:2,heart:112,work:''}
